a9bb728734082bf0dc5c1e2f3849502f46a24844
Managatrix <mana.tomfd@gmail.com>
2023-06-13 08:57:50 -0700

    Using MarkupString for MP descriptions

--- Objects/Project.cs
+++ Objects/Project.cs
@@ -6,14 +6,7 @@ public class Project
     public string[] DescriptionParagraphs { get; set; }
     public string ImagePath { get; set; }
     public string GithubLink { get; set; }
-    public Project(string name, string imagePath, string githubLink, string paragraph1, string paragraph2 = "", string paragraph3 = "")
-    {
-        Name = name;
-        ImagePath = imagePath;
-        GithubLink = githubLink;
-        string[] rawDescriptionParagraphs = { paragraph1, paragraph2, paragraph3 };
-        DescriptionParagraphs = rawDescriptionParagraphs.Where(x => !string.IsNullOrEmpty(x)).ToArray();
-    }
+    
     [JsonConstructor]
     public Project(string name, string imagePath, string githubLink, string[] descriptionParagraphs)
     {

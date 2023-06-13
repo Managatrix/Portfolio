52e99ecac7997bbe0583876cc4de49d541fa1793
Managatrix <mana.tomfd@gmail.com>
2023-06-13 10:37:49 -0700

    Changed Project object structure

--- Objects/Project.cs
+++ Objects/Project.cs
@@ -3,23 +3,23 @@ using System.Text.Json.Serialization;
 public class Project
 {
     public string Name { get; set; }
-    public string[] DescriptionParagraphs { get; set; }
+    public string Description { get; set; }
     public string ImagePath { get; set; }
     public string GithubLink { get; set; }
-    
+
     [JsonConstructor]
-    public Project(string name, string imagePath, string githubLink, string[] descriptionParagraphs)
+    public Project(string name, string imagePath, string githubLink, string description)
     {
         Name = name;
         ImagePath = imagePath;
         GithubLink = githubLink;
-        DescriptionParagraphs = descriptionParagraphs;
+        Description = description;
     }
     public Project()
     {
         Name = "";
         ImagePath = "";
         GithubLink = "";
-        DescriptionParagraphs = new string[] { };
+        Description = "";
     }
 }

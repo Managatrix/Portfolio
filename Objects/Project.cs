3c56fc8b8dcea71436e08cc6a919fb0bca934594
Managatrix <mana.tomfd@gmail.com>
2023-06-12 21:29:58 -0700

    Completed MP Json architecture

--- Objects/Project.cs
+++ Objects/Project.cs
@@ -1,15 +1,32 @@
+using System.Text.Json.Serialization;
+
 public class Project
 {
-    public string Name { get; }
-    public string[] DescriptionParagraphs { get; }
-    public string ImagePath { get; }
-    public string GithubLink { get; }
+    public string Name { get; set; }
+    public string[] DescriptionParagraphs { get; set; }
+    public string ImagePath { get; set; }
+    public string GithubLink { get; set; }
     public Project(string name, string imagePath, string githubLink, string paragraph1, string paragraph2 = "", string paragraph3 = "")
     {
         Name = name;
-        ImagePath = "images/" + imagePath;
+        ImagePath = imagePath;
         GithubLink = githubLink;
-        string[] rawDescriptionParagraphs = {paragraph1, paragraph2, paragraph3};
+        string[] rawDescriptionParagraphs = { paragraph1, paragraph2, paragraph3 };
         DescriptionParagraphs = rawDescriptionParagraphs.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+    }
+    [JsonConstructor]
+    public Project(string name, string imagePath, string githubLink, string[] descriptionParagraphs)
+    {
+        Name = name;
+        ImagePath = imagePath;
+        GithubLink = githubLink;
+        DescriptionParagraphs = descriptionParagraphs;
+    }
+    public Project()
+    {
+        Name = "";
+        ImagePath = "";
+        GithubLink = "";
+        DescriptionParagraphs = new string[] { };
     }
 }

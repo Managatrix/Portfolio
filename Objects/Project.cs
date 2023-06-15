56399c47b8dd15eb94c43829d09074a7856076d6
Managatrix <mana.tomfd@gmail.com>
2023-06-15 14:50:05 -0700

    Added 2nd project and minor corrections

--- Objects/Project.cs
+++ Objects/Project.cs
@@ -3,14 +3,16 @@ using System.Text.Json.Serialization;
 public class Project
 {
     public string Name { get; set; }
+    public string Icon { get; set; }
     public string Description { get; set; }
     public string ImagePath { get; set; }
     public string GithubLink { get; set; }
 
     [JsonConstructor]
-    public Project(string name, string imagePath, string githubLink, string description)
+    public Project(string name, string icon, string imagePath, string githubLink, string description)
     {
         Name = name;
+        Icon = icon;
         ImagePath = imagePath;
         GithubLink = githubLink;
         Description = description;
@@ -18,6 +20,7 @@ public class Project
     public Project()
     {
         Name = "";
+        Icon = "";
         ImagePath = "";
         GithubLink = "";
         Description = "";

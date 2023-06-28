439fadb075b3b6a0888b60be5ded6a5c8d2c4852
Managatrix <Managatrix@users.noreply.github.com>
2023-06-28 18:18:55 +0000

    Deploy to GitHub pages

---
+++ js/ScrollToId.js
@@ -0,0 +1,12 @@
+// Source:  https://www.meziantou.net/anchor-navigation-in-a-blazor-application.htm
+
+function BlazorScrollToId(id) {
+    const element = document.getElementById(id);
+    if (element instanceof HTMLElement) {
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        });
+    }
+}
